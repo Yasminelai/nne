a7021389c1b1bc507b8202acee6f9e398f48efa9
xlai <xlai@itu.dk>
2024-10-17 21:49:42 +0200

    preprocess1

--- preprocess/check.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import pandas as pd
-
-data = pd.read_csv('/Users/shiroixy/copenhagen/NNE/InnoTech3D/data/TrainingSet/Project1/labeled_point_cloud.csv')
-data['Label'].value_counts()
