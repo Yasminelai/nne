5c0aef6132745e65ffde68126f0beffaaf703e9a
xlai <xlai@itu.dk>
2024-10-17 21:47:33 +0200

    preprocess

--- preprocess/label_point.py
+++ preprocess/label_point.py
@@ -1,39 +1,44 @@
 import numpy as np
 import pandas as pd
+import os
 
-def load_point_cloud(file_path):
-    return np.loadtxt(file_path)
+point_cloud = np.loadtxt('Project1.xyz')
 
-point_cloud = load_point_cloud('/Users/shiroixy/copenhagen/NNE/InnoTech3D/data/TrainingSet/Project1/Project1.xyz')
-point_cloud = np.random.permutation(point_cloud)[:1000]
-data = pd.read_csv('/Users/shiroixy/copenhagen/NNE/InnoTech3D/data/TrainingSet/Project1/Project1.csv')
+N = point_cloud.shape[0]
+random_indices = np.random.choice(N, 1000, replace=False)
+sampled_points = point_cloud[random_indices]
 
+point_labels = np.zeros(1000)
 
-def label_points(point_cloud, df):
-    labels = []
-    for point in point_cloud:
-        x, y, z = point[:3]
-        point_label = 'Unlabeled'  
+metadata = pd.read_csv('Project1.csv')
+object_ids = metadata['ID'].values
+labels = metadata[' Label'].values
+
+label_mapping = {
+    'Pipe': 1,
+    'HVAC_Duct': 2,
+    'Structural_IBeam': 3,
+    'Structural_ColumnBeam': 4
+}
+
+mask_folder = 'Project1.masks'
+mask_files = os.listdir(mask_folder)
+
+for mask_file in mask_files:
+    mask_path = os.path.join(mask_folder, mask_file)
+    object_id = os.path.splitext(mask_file)[0].replace('mask', '')
+
+    label_idx = np.where(object_ids == object_id)[0]
+    if len(label_idx) > 0:
+        label = labels[label_idx[0]]
+        numeric_label = label_mapping[label]
         
-        for _, row in df.iterrows():
-            if (row[' BB.Min.X '] <= x <= row[' BB.Max.X '] and
-                row[' BB.Min.Y '] <= y <= row[' BB.Max.Y '] and
-                row[' BB.Min.Z '] <= z <= row[' BB.Max.Z']):
-                point_label = row[' Label']
-                break
-        
-        labels.append(point_label)
-    
-    return labels
+        mask_points = np.loadtxt(mask_path, dtype=int)
+        mask_points_set = set(mask_points)
+        for idx_pos, idx in enumerate(random_indices):
+            if idx in mask_points_set:
+                point_labels[idx_pos] = numeric_label
 
-point_labels = label_points(point_cloud, data)
+sampled_data_with_labels = np.concatenate([sampled_points, point_labels[:, np.newaxis]], axis=1)
 
-
-labeled_points_df = pd.DataFrame(point_cloud, columns=['X', 'Y', 'Z', 'I', 'R', 'G', 'B'])
-labeled_points_df['Label'] = point_labels
-
-labeled_points_df.head(100)
-
-
-output_file = 'labeled_point_cloud.csv'
-labeled_points_df.to_csv(output_file, index=False)
+np.save('sampled_pointnet_input_with_labels.npy', sampled_data_with_labels)
